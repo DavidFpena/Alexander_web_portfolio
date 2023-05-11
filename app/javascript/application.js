df585c524fa3bd3ff327e23df5410a13d12e3a34
david felipe pena <david.felipe.pena@hotmail.com>
2023-05-11 12:21:42 -0400

    landing page setup updated

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,33 @@
 import { Turbo } from "@hotwired/turbo-rails"
 Turbo.session.drive = false
 import "controllers"
+
+const dropdownContainer = document.querySelector('.text-cont-port');
+const dropdownButton = dropdownContainer.querySelector('#dropdown-button');
+const dropdownMenu = dropdownContainer.querySelector('#dropdown-menu');
+
+dropdownContainer.addEventListener('mouseenter', () => {
+  dropdownMenu.classList.add('show');
+  dropdownButton.setAttribute('aria-expanded', 'true');
+});
+
+dropdownContainer.addEventListener('mouseleave', () => {
+  dropdownMenu.classList.remove('show');
+  dropdownButton.setAttribute('aria-expanded', 'false');
+});
+
+
+const scrollToTopBtn = document.getElementById("backtotopsymbol");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+scrollToTopBtn.addEventListener("click", function() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,36 +7,3 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
-
-
-
-
-const dropdownContainer = document.querySelector('.text-cont-port');
-const dropdownButton = dropdownContainer.querySelector('#dropdown-button');
-const dropdownMenu = dropdownContainer.querySelector('#dropdown-menu');
-
-dropdownContainer.addEventListener('mouseenter', () => {
-  dropdownMenu.classList.add('show');
-  dropdownButton.setAttribute('aria-expanded', 'true');
-});
-
-dropdownContainer.addEventListener('mouseleave', () => {
-  dropdownMenu.classList.remove('show');
-  dropdownButton.setAttribute('aria-expanded', 'false');
-});
-
-
-const scrollToTopBtn = document.getElementById("backtotopsymbol");
-
-window.onscroll = function() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    scrollToTopBtn.style.display = "block";
-  } else {
-    scrollToTopBtn.style.display = "none";
-  }
-};
-
-scrollToTopBtn.addEventListener("click", function() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-});
